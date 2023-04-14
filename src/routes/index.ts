e6190c967689b10772f86b3ad85ea306bbbe8561
Paul Adeoye <oluwadamilare321@gmail.com>
2023-04-15 00:56:26 +0100

    routes file

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-shadow */
+export enum Routes {
+  SIGNIN = '/auth/signin',
+  SIGNUP = '/auth/signup',
+  FORGOT_PASSWORD = '/auth/forgot-password',
+  RESET_PASSWORD = '/auth/reset-password',
+  HOME = '/',
+  FORUM = '/forum',
+  STAFF_LIST = '/staff-list',
+  ABOUT = '/about',
+  CONTACT = '/contact',
+  DASHBOARD = '/dashboard',
+}
