efb4d47c9baa464976189d7b2a8545cca3572c1c
Paul Adeoye <oluwadamilare321@gmail.com>
2023-04-15 01:09:46 +0100

    home, about page init

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,14 +2,14 @@
 
 import { Link, Text } from '@chakra-ui/react';
 
-export default function Page() {
+export default function About() {
   return (
     <>
       <Link href="/about" color="blue.400" _hover={{ color: 'blue.500' }}>
         <Text textAlign="center">About</Text>
       </Link>
       <Text color="red" textAlign="center">
-        This is the homepage. My Name is Paul King and I cook sweet beans
+        This is the about page. My Name is Paul King and I cook sweet beans
       </Text>
     </>
   );
