acfa50a0b4bd400b6a90e4a35f64b74016e042b0
Paul Adeoye <oluwadamilare321@gmail.com>
2023-04-20 02:36:53 +0100

    fix: navbar updated

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,35 +1,14 @@
 'use client';
 
 import { Flex, Text, Link, Button } from '@chakra-ui/react';
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
-import { useState } from 'react';
 import logo from '../../../public/logo.png';
 import { Routes } from '../../routes';
 
 export default function Navbar() {
-  const [activeTab, setActiveTab] = useState('Home');
-  const links = [
-    {
-      title: 'Home',
-      path: Routes.HOME,
-    },
-    {
-      title: 'About',
-      path: Routes.ABOUT,
-    },
-    {
-      title: 'Staff list',
-      path: Routes.STAFF_LIST,
-    },
-    {
-      title: 'Forum',
-      path: Routes.FORUM,
-    },
-    {
-      title: 'Contact',
-      path: Routes.CONTACT,
-    },
-  ];
+  const pathname = usePathname();
+  const primary = 'primary.200';
 
   return (
     <Flex
@@ -45,31 +24,61 @@ export default function Navbar() {
         <Image src={logo} alt="logo" />
       </Flex>
       <Flex columnGap="48px">
-        {links.map(link => (
-          <Flex
-            onClick={() => {
-              console.log('activetab', activeTab);
-              setActiveTab(link.title);
-            }}
-            key={link.title}
-          >
-            <Link
-              href={link.path}
-              // color="white"
-              _hover={{ color: 'primary.200' }}
+        <Flex>
+          <Link href={Routes.HOME} _hover={{ color: primary }}>
+            <Text
+              textAlign="center"
+              fontWeight="bold"
+              style={{ color: pathname === '/' ? primary : 'white' }}
             >
-              <Text
-                style={{
-                  color: activeTab === link.title ? 'primary.200' : 'white',
-                }}
-                textAlign="center"
-                fontWeight="bold"
-              >
-                {link.title}
-              </Text>
-            </Link>
-          </Flex>
-        ))}
+              Home
+            </Text>
+          </Link>
+        </Flex>
+        <Flex>
+          <Link href={Routes.ABOUT} _hover={{ color: primary }}>
+            <Text
+              textAlign="center"
+              fontWeight="bold"
+              style={{ color: pathname === '/about' ? primary : 'white' }}
+            >
+              About
+            </Text>
+          </Link>
+        </Flex>
+        <Flex>
+          <Link href={Routes.STAFF_LIST} _hover={{ color: primary }}>
+            <Text
+              textAlign="center"
+              fontWeight="bold"
+              style={{ color: pathname === '/staff-list' ? primary : 'white' }}
+            >
+              Staff list
+            </Text>
+          </Link>
+        </Flex>
+        <Flex>
+          <Link href={Routes.FORUM} _hover={{ color: primary }}>
+            <Text
+              textAlign="center"
+              fontWeight="bold"
+              style={{ color: pathname === '/forum' ? primary : 'white' }}
+            >
+              Forum
+            </Text>
+          </Link>
+        </Flex>
+        <Flex>
+          <Link href={Routes.CONTACT} _hover={{ color: primary }}>
+            <Text
+              textAlign="center"
+              fontWeight="bold"
+              style={{ color: pathname === '/contact' ? primary : 'white' }}
+            >
+              Contact
+            </Text>
+          </Link>
+        </Flex>
       </Flex>
       <Link href={Routes.SIGNIN}>
         <Button px="40px" variant="primary">
